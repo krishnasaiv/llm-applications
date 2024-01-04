7925beb88cb65b72b126ef502a7825018dbdd348
krishnasaiv <vootla.sai@gmail.com>
2024-01-04 17:28:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -225,15 +225,15 @@ if __name__ == "__main__":
     q = st.text_input("Ask a question about the content of your file:")
     if q:
         st.session_state.history.append(HumanMessage(content=q))
-        if 'vs' in st.session_state:
+        # if 'vs' in st.session_state:
         # with st.spinner('Working on your request ...'):
-            vector_store = st.session_state.vs 
-            # st.write(type(vector_store), vector_store is None)
-            # st.write(type(st.session_state.vs), st.session_state.vs is None)
-            
-            llmchain = get_llm_chain(vector_store=vector_store , model_name=model_name, openai_api_key=api_key)
-            answer, chat_history = ask(query=q, llm_chain=llmchain)
-            st.session_state.history.append(AIMessage(content=answer))
+        vector_store = st.session_state.vs 
+        # st.write(type(vector_store), vector_store is None)
+        # st.write(type(st.session_state.vs), st.session_state.vs is None)
+        
+        llmchain = get_llm_chain(vector_store=vector_store , model_name=model_name, openai_api_key=api_key)
+        answer, chat_history = ask(query=q, llm_chain=llmchain)
+        st.session_state.history.append(AIMessage(content=answer))
 
             # st.text_area('LLM Answer: ', value=answer)
 
