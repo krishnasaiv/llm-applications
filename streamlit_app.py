5e1c81975b03075816c3c6d350e160d1f1a3050d
krishnasaiv <vootla.sai@gmail.com>
2024-01-05 19:29:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ def calculate_embedding_cost(texts):
     import tiktoken
     enc = tiktoken.encoding_for_model('text-embedding-ada-002')
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
-    return f"Total Tokens: {total_tokens} |  Cost: {total_tokens / 1000 * 0.0004}"
+    return f"Total Tokens: {total_tokens} |  Cost: {round(total_tokens / 1000 * 0.0004, 3)} $"
 
 
 #################################
